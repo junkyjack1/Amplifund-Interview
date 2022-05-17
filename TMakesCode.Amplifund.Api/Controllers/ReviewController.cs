d9c07fa95fc1dbc0ddf130ca08058b3266c36c52
T. Shokrie <junkyjack1@gmail.com>
2022-05-17 14:20:30 -0400

    more scaffolding

---
+++ TMakesCode.Amplifund.Api/Controllers/ReviewController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TMakesCode.Amplifund.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+    }
+}
