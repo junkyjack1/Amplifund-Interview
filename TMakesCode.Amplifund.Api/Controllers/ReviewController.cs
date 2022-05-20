c5024ca4ccf465d4c88717a98c513a8668dd7044
T. Shokrie <junkyjack1@gmail.com>
2022-05-20 10:56:07 -0400

    prepping for deploy to azure

--- TMakesCode.Amplifund.Api/Controllers/ReviewController.cs
+++ TMakesCode.Amplifund.Api/Controllers/ReviewController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using TMakesCode.Amplifund.Biz.Reviews.Contracts;
 using TMakesCode.Amplifund.Domain;
 
