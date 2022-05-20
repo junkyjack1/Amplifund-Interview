52aa35b6bb47fdb15db8d962a7edd46f598377a9
T. Shokrie <junkyjack1@gmail.com>
2022-05-20 11:55:43 -0400

    added cors

--- TMakesCode.Amplifund.Api/Program.cs
+++ TMakesCode.Amplifund.Api/Program.cs
@@ -23,6 +23,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials() // allow credentials
+); 
+
 app.UseAuthorization();
 
 app.MapControllers();
